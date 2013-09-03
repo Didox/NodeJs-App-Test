37960e9f6db1624a21b661dada6ee280ea872469
Didox <dansantos@abril.com.br>
2013-09-03 11:15:31 -0300

    criando rota

--- app_nodejs/app.js
+++ app_nodejs/app.js
@@ -31,6 +31,10 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/users', user.list);
 
+app.get('/test-rota', function(req, res){
+  res.render('test_rota', { usuario: {nome:'leo', slug:'papa-man'} });
+});
+
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
